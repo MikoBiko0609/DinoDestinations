9fe4f1b0fe0bf37962b3bf0bd68fae50301beb18
MikoBiko0609 <your.mad3566@icloud.com>
2025-01-21 16:10:12 -0600

    trying to make api work in front end

---
+++ frontend/resultPage.js
@@ -0,0 +1,8 @@
+// In resultPage.js
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+// Use the apiKey variable to load the Google Maps API
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
+script.async = true;
+document.body.appendChild(script);
