6f88a7469691faeeddc46163448da5a6b624ecad
MikoBiko0609 <your.mad3566@icloud.com>
2025-01-21 16:22:49 -0600

    updating results page

--- frontend/resultPage.js
+++ frontend/resultPage.js
@@ -1,4 +1,4 @@
 // In resultPage.js
 require('dotenv').config();
 const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-console.log('Google Maps API Key:', apiKey);
+console.log('Google Maps API Key:', apiKey);a
