04dbaa139575a59f23be11295b243f4e93666bc5
MikoBiko0609 <your.mad3566@icloud.com>
2025-01-21 16:18:05 -0600

    testing if api is accesible

--- frontend/resultPage.js
+++ frontend/resultPage.js
@@ -1,9 +1,4 @@
 // In resultPage.js
 require('dotenv').config();
 const apiKey = process.env.GOOGLE_MAPS_API_KEY;
-
-// Use the apiKey variable to load the Google Maps API
-const script = document.createElement('script');
-script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
-script.async = true;
-document.body.appendChild(script);
+console.log('Google Maps API Key:', apiKey);
